c2e695ea2dfbbb5e2c43d124127244e9590d5d9a
Gleriston Castro <xpanium@gmail.com>
2025-11-29 23:46:44 -0300

    feat: add animation components
    - Added page transitions
    - Added micro-interactions
    - Added celebrate effects
    - Added counters and scroll

---
+++ components/animations/index.ts
@@ -0,0 +1,32 @@
+// Page transitions
+export {
+  PageTransition,
+  StaggerContainer,
+  StaggerItem,
+  FadeIn,
+  SlideIn,
+  ScaleIn,
+} from "./page-transitions";
+
+// Micro-interactions
+export {
+  AnimatedCard,
+  AnimatedButton,
+  Floating,
+  Pulse,
+  Shake,
+  Rotate,
+  HoverLift,
+} from "./micro-interactions";
+
+// Celebrate effects
+export { Celebrate, ConfettiExplosion, Sparkle } from "./celebrate";
+
+// Counters and scroll
+export {
+  AnimatedCounter,
+  AnimatedCurrency,
+  AnimatedPercentage,
+  ScrollReveal,
+  useCountUp,
+} from "./counters";
